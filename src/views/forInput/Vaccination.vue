69627208892cd8fc10ba77db03670888a96a35c4
Aruzhan Edilova <avolide@gmail.com>
2019-03-07 21:42:33 +0600

    add pagination to "vaccination"

--- src/views/forInput/Vaccination.vue
+++ src/views/forInput/Vaccination.vue
@@ -28,6 +28,14 @@
                         </tr>
                         </tbody>
                     </table>
+                    <div class="pagination d-flex flex-row justify-content-between w-100">
+                        <b-col cols="2" sm="4" md="2" class="mb-3 mb-xl-0 ">
+                            <b-button block variant="primary" class="mb-2" @click="pagebtnPrevious">Пред</b-button>
+                        </b-col>
+                        <b-col cols="2" sm="4" md="2" class="mb-3 mb-xl-0 ">
+                            <b-button block variant="primary" class="mb-2" @click="pagebtnNext">След</b-button>
+                        </b-col>
+                    </div>
                 </b-card>
             </div>
         </b-col>
@@ -42,12 +50,68 @@
         data: () => {
             return {
                 getMedicine: "",
+                linkPrev:"",
+                linkNext:"",
             }
         },
         created: function () {
             this.fetchData();
         },
         methods: {
+            pagebtnPrevious(){
+                return new Promise((resolve, reject) => {
+                    // commit('auth_request')
+                    console.log('promise example ');
+                    const token = sessionStorage.getItem('token');
+                    axios.defaults.headers.common['Authorization'] = "Token " + token
+                    console.log(axios.defaults.headers, 'headers in get');
+
+                    //get Department
+                    axios({url: this.linkPrev, method: 'GET' })
+
+                        .then(resp => {
+                            console.log('getMedicine',resp.data)
+                            this.getMedicine = resp.data.results
+                            this.linkPrev = resp.data.links.previous
+                            this.linkNext = resp.data.links.next
+                            // this.getFarmerLength = resp.data.count % 100
+                            console.log('LENGTHHHHH', resp.data.count)
+                            resolve(resp)
+
+
+                        })
+                        .catch(err => {console.log(err)
+                        })
+
+                })
+            },
+            pagebtnNext(){
+                return new Promise((resolve, reject) => {
+                    // commit('auth_request')
+                    console.log('promise example ');
+                    const token = sessionStorage.getItem('token');
+                    axios.defaults.headers.common['Authorization'] = "Token " + token
+                    console.log(axios.defaults.headers, 'headers in get');
+
+                    //get Department
+                    axios({url: this.linkNext, method: 'GET' })
+
+                        .then(resp => {
+                            console.log('getMedicine',resp.data)
+                            this.getMedicine = resp.data.results
+                            this.linkPrev = resp.data.links.previous
+                            this.linkNext = resp.data.links.next
+                            // this.getFarmerLength = resp.data.count % 100
+                            console.log('LENGTHHHHH', resp.data.count)
+                            resolve(resp)
+
+
+                        })
+                        .catch(err => {console.log(err)
+                        })
+
+                })
+            },
             fetchData: function () {
                 return new Promise((resolve, reject) => {
                     // commit('auth_request')
@@ -61,6 +125,8 @@
                         .then(resp => {
                             console.log('getMedicine',resp.data)
                             this.getMedicine = resp.data.results
+                            this.linkPrev = resp.data.links.previous
+                            this.linkNext = resp.data.links.next
                             resolve(resp)
 
 
